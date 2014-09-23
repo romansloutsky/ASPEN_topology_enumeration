db67978158c7820bdb72af0dc01e7f09cc418bb1
Roman Sloutsky <devnull@localhost>
2014-09-23 14:03:11 -0500

    Wrapper method generate_extensions() returns extended assemblies or None if none

--- topolenum/topolenum.py
+++ topolenum/topolenum.py
@@ -646,13 +646,12 @@ class TreeAssembly(object):
   def generate_extensions(self,encountered_assemblies,min_score=None):
     new_pairs,joins,attachments = self.find_extensions(encountered_assemblies,min_score=None)
     if any((new_pairs,joins,attachments)):
-      # Just a placeholder call to a stub of a function right now: not sure what and how it will return
-      self.build_extensions(new_pairs, joins, attachments)
       # Return built extensions and any additional information
+      return self.build_extensions(new_pairs, joins, attachments)
     else:
       # Somehow inform caller that this assembly is unextendable - it is the caller's responsibility to
       # remove this assembly from the container of active assemblies
-      pass
+      return None
 
 def assemble_histtrees(pwhist,leaves_to_assemble,num_requested_trees=1000,freq_cutoff=0.9,max_iter=100000,processing_bite_size=10000):
     pwindiv = [(pair[0],score) for pair in [(f,[dd for i,dd in enumerate(ds)
